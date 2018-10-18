c0f23707e3863e83a010133c9af75061ec14b04a
blancont <blancont@rose-hulman.edu>
2018-10-18 10:28:22 -0400

    m1

--- src/m1_sequence_mutation.py
+++ src/m1_sequence_mutation.py
@@ -5,8 +5,8 @@ This problem provides practice at:
   ***  LOOPS WITHIN LOOPS, SEQUENCES and MUTATION  ***
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Nathaniel Blanco.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -31,9 +31,20 @@ def run_test_zero_changer():
     print('  Actual:  ', test1)
 
     # ------------------------------------------------------------------
-    # TODO: 2. Write at least 2 additional tests for the  zero_changer   function.
+    # DONE: 2. Write at least 2 additional tests for the  zero_changer   function.
     #   Try do do some unexpected things like empty lists or an empty tuple.
     # ------------------------------------------------------------------
+    test2 = ()
+    zero_changer(test2)
+    expect = ()
+    print('Expected:', expect)
+    print('Answer:  ', test2)
+
+    test3 = ([1, 0], [], [0, 100, 0])
+    zero_changer(test3)
+    expect = ([1, 1], [], [2, 100, 3])
+    print('Expected:', expect)
+    print('Answer:  ', test3)
 
 
 def zero_changer(tuple_of_lists):
@@ -61,7 +72,7 @@ def zero_changer(tuple_of_lists):
       :type tuple_of_lists: tuple of list[int]
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # ------------------------------------------------------------------
     # ------------------------------------------------------------------
@@ -69,6 +80,12 @@ def zero_changer(tuple_of_lists):
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:  10 minutes.
     # ------------------------------------------------------------------
+    replacement_number = 1
+    for list_in_tuple in range(len(tuple_of_lists)):
+        for index_of_list_in_tuple in range(len(tuple_of_lists[list_in_tuple])):
+            if tuple_of_lists[list_in_tuple][index_of_list_in_tuple] == 0:
+                tuple_of_lists[list_in_tuple][index_of_list_in_tuple] = replacement_number
+                replacement_number = replacement_number + 1
 
 
 # ----------------------------------------------------------------------
